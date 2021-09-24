6256af64030b091e2759d0582edd62d2b0d688d9
BrayanVillalba <63078111+BrayanVillalba@users.noreply.github.com>
2021-09-24 14:46:12 -0500

    Add files via upload

---
+++ capaEntidades/clsProducto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace capaEntidades
+{
+    public class clsProducto
+    {
+        private int codigo;
+        private string nombre;
+        private string descripcion;
+        private double precio;
+        private string descuento;
+
+        public int Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public double Precio { get; set; }
+        public double Descuento { get; set; }
+    }
+}
